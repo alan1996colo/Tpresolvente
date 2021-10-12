f4430d654578c95b3bd8f8737c680d1ac214116c
alan1996colo <alanechabarri@gmail.com>
2021-10-12 18:27:32 -0300

    falta averiguar como recorrer el arreglo desde asm, porque al parecer obtiene la direccion de memoria

--- tpFormResolv.c
+++ tpFormResolv.c
@@ -12,6 +12,8 @@ extern float SUMAR(float a, float b);/*suma los dos valores pasados por parametr
  retorna el resultado*/
 extern float RAIZ2(float a);/*hace raiz cuadrada de lo pasado por parametro y lo retorna*/
 extern float DIVIDIR(float a,float b);/*divide a /b y lo retorna*/
+
+extern float PRODUCTOESCALAR(float r,float vectores[]);
 /*extern float float_b;
 extern float float_mb;
 extern float bALcuadrado;
@@ -20,8 +22,15 @@ extern float float_a;
 extern float float_c;
 ¿para que hacerlas global si puedo administrar memoria desde C?
 */
-
+void p(int cant,float vectores[]){
+for(int j=0;j<cant;j++){
+		printf("\n %f \n",vectores[j]);
+	}return ;};
+	//visualizamos el vector
+	
+	
 int main (){
+	
 float ac4;//tiene el valor -4ac
 float bALcuadrado;
 float mb;//menos b
@@ -33,7 +42,41 @@ float b=1;
 float c=-2;
 float x2;
 float x1;
+int cantCordenadas=3;
 
+
+//int cantVectores=2;
+//nos damos cuenta que solo pedia UN solo vector
+//printf("\n ingrese la cantidad de vectores que va emplear");
+//scanf("%i",&cantVectores);
+printf("\n ingrese la cantidad de coordenadas que va usar ");
+scanf("%i",&cantCordenadas);
+float vectores[cantCordenadas];
+//float vectores [cantVectores][cantCordenadas];
+//creamos la matriz
+//for (int i=0;i<cantVectores;i++){
+for(int j=0;j<cantCordenadas;j++){
+	printf("\n ingrese el valor de la cordenada %i  :",j+1);
+	//printf("  en el vector %i :",i+1);
+	scanf("%f",&vectores[j]);
+}
+
+p(cantCordenadas,vectores); //vemos el vector
+
+/*En matemáticas, el producto escalar,también conocido como producto interno o 
+producto punto, es una operación algebraica que toma dos secuencias de números 
+de igual dimensión (usualmente en la forma de vectores) y "retorna un único número".
+*/
+a=PRODUCTOESCALAR(2.0,vectores);
+imprimirasm(a);
+
+//a=p(2.3,vectores);
+
+
+
+//Por lo tanto debe multiplicar cada coordenada * R 
+//y retornar la suma de esos productos.
+/* 
 printf("ingrese el valor a");
 scanf("\n %f" ,&a);
 printf("ingrese el valor b");
